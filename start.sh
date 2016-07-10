2ee5bb0937607447c03c2500a158baee5d0e9b97
moreira_pais@hotmail.com <moreira_pais@hotmail.com>
2016-07-10 21:24:53 +0100

    save changes

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo run this again 3.
+#/bin/bash -c -v "java -jar /opt/dropwizard/LetsGetDigital-0.0.1-SNAPSHOT.jar server /opt/dropwizard/config_aws.yml"
+#/bin/sh -c "java -jar /opt/dropwizard/LetsGetDigital-0.0.1-SNAPSHOT.jar server /opt/dropwizard/config_aws.yml"
+/bin/sh -c "java -jar  -Dspring.profiles.active=prod /opt/dropwizard/com.LetsGetDigitalCassandra-0.0.1-SNAPSHOT.jar"
